6ac9110cc1b5528ff4357bd5d0e4a9b5f95db318
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-10-18 15:35:42 +0400

    started optional binary search exercises

---
+++ 4-week/optional/search_rev_arr.py
@@ -0,0 +1,51 @@
+def search(nums, target):
+    if len(nums) == 1:
+        return 0 if nums[0] == target else -1
+
+    if len(nums) == 2:
+        if nums[0] == target:
+            return 0
+        elif nums[1] == target:
+            return 1
+        else:
+            return -1
+
+    is_pivot = True
+    lo, hi = 0, len(nums)-1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+
+        if mid == len(nums)-1:
+            right = len(nums)-1
+            left = 0
+            is_pivot = False
+            break
+        if nums[mid] > nums[mid+1]:
+            hi = mid-1
+        else:
+            lo = mid+1
+
+    if is_pivot:
+        if target < nums[lo]:
+            left = lo
+            right = len(nums)-1
+        else:
+            left = 0
+            right = lo
+    else:
+        left, right = 0, len(nums)-1
+
+    print(left, right, is_pivot)
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:  # a[mid] > target
+            left = mid + 1
+
+    return -1
+
+print(search([3,5,1], 1))
