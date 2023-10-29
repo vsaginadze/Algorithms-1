c482dc15714395a57ca74fe79665641855c87eda
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-10-29 04:20:19 +0400

    merge sort

--- 5-week/merge_sort.py
+++ 5-week/merge_sort.py
@@ -17,8 +17,18 @@ def merge(a, b, c):
 
     print(c)
 
-a = [3, 5]
-b = [6, -1, 11]
+a = [3, 5, 7, 10]
+b = [-1, 6, 11]
 c = (len(a) + len(b)) * [0]
 
 merge(a, b, c)
+
+'''
+a and b are sorted
+1. i and j tracks smallest elements of a and b respectively
+
+2. if i hits len(a) there are no more smallest elements of a so we add smallest elements of b
+3. same as step 2 in j
+4. if a's smallest element is smaller than b's smallest element than add from a and increase the i(index of a smallest element of a)
+5. if step 4 fails do same for b
+'''
