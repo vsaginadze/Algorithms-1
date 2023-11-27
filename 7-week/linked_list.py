3a2715a494c8c2471d3a72c02438b78740a48cba
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-28 01:43:09 +0400

    linked list functions

--- 7-week/linked_list.py
+++ 7-week/linked_list.py
@@ -5,3 +5,30 @@ class Node:
         self.val = val
         self.next = next
 
+def list_len(head):
+    n = head
+    count = 0
+
+    while n != None:
+        n = n.next
+        count += 1
+    
+    return count
+
+def list_sum(head):
+    n = head
+    sum = 0
+
+    while n != None:
+        sum += n.val
+        n = n.next
+    
+    return sum
+
+def list_1_to_n(n):
+    head = None
+    for i in range(n, 0, -1):
+        p = Node(i, head)
+        head = p
+
+    return head
