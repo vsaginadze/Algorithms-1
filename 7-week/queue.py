bc8dec6c5bceda9e98b147d71db9978676047c37
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-11-29 22:20:18 +0400

    linkedstack

---
+++ 7-week/queue.py
@@ -0,0 +1,3 @@
+# ArrayQueue
+
+class Array
--- 7-week/stack.py
+++ 7-week/stack.py
@@ -1,16 +1,40 @@
 # Array Stack
 
-class ArrayStack:
+# class ArrayStack:
+#     def __init__(self):
+#         self.a = []
+
+#     def is_empty(self):
+#         return len(self.a) == 0
+
+#     def push(self, x):
+#         self.a.append(x)
+
+#     def pop(self):
+#         assert not self.a.is_empty(), "stack is empty"
+#         return self.a.pop()
+
+
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+
+# Linked Stack
+
+class LinkedStack:
     def __init__(self):
-        self.a = []
-
+        self.head = None
+    
     def is_empty(self):
-        return len(self.a) == 0
+        return self.head == None
 
-    def push(self, x):
-        self.a.append(x)
+    def push(self, val):
+        node = Node(val, self.head)
+        self.head = node
 
     def pop(self):
-        assert not self.a.is_empty(), "stack is empty"
-        return self.a.pop()
-
+        assert self.head != None, "stack is empty"
+        x = self.head.val
+        self.head = self.head.next
+        return x
