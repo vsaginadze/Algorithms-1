8c1844645ef6d148017a6ea9b70f74674ecf518d
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-12-14 00:40:20 +0400

    finish 8-week 2-exercise

--- 8-week/exercises/1.py
+++ 8-week/exercises/1.py
@@ -2,74 +2,17 @@
 Write a function completeTree(h) that builds a complete binary tree of height h. 
 Every node's value should be that node's depth.
 '''
-
-class Node:
-    def __init__(self, key, left, right):
-        self.key = key
-        self.left = left
-        self.right = right
-
-    def display(self):
-            lines, *_ = self._display_aux()
-            for line in lines:
-                print(line)
-
-    def _display_aux(self):
-        """Returns list of strings, width, height, and horizontal coordinate of the root."""
-        # No child.
-        if self.right is None and self.left is None:
-            line = '%s' % self.key
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if self.right is None:
-            lines, n, p, x = self.left._display_aux()
-            s = '%s' % self.key
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if self.left is None:
-            lines, n, p, x = self.right._display_aux()
-            s = '%s' % self.key
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.left._display_aux()
-        right, m, q, y = self.right._display_aux()
-        s = '%s' % self.key
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
+from node import Node
 
 def completeTree(h, d = 0):
-    if h == 0:
-        return None
+    if h == d: return None
     
-    l = completeTree(h - 1, d + 1)
-    r = completeTree(h - 1, d + 1)
+    l = completeTree(h, d + 1)
+    r = completeTree(h, d + 1)
 
     return Node(d, l, r)
 
-    
 
-node = completeTree(8)
+node = completeTree(4)
 node.display()
 
---
+++ 8-week/exercises/2.py
@@ -0,0 +1,27 @@
+'''Write a function mirror(t) that modifies a binary tree, 
+flipping it from left to right so that it looks like a mirror image of the original tree.'''
+
+
+from node import Node
+ 
+
+def mirror(t):
+    if t == None:
+        return
+    
+    temp = t.left
+    t.left = t.right
+    t.right = temp
+
+    mirror(t.right)
+    mirror(t.left)
+
+
+l = Node(2)
+r = Node(3, Node(6), Node(7))
+root = Node(1, l, r)
+
+root.display()
+mirror(root)
+print("--------")
+root.display()
