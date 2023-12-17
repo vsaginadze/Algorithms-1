960d26e2937b24c67b2f6be469d2832b31d09d1c
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2023-12-17 22:08:22 +0400

    finished exercise 3 week 8

---
+++ 8-week/exercises/3b.py
@@ -0,0 +1,25 @@
+'''
+b) Modify the function to be more efficient if the input tree is known to be a binary search tree.
+'''
+
+from node import Node
+
+def max_in_tree(t):
+    while t:
+        if t.right:
+            t = t.right
+        else:
+            return t.val
+
+
+root = Node(5, Node(3, Node(2), Node(4)))
+
+l = Node(3, Node(2), Node(6, Node(5), Node(8)))
+r = Node(20, Node(15, None, Node(18)), Node(23))
+root = Node(10, l, r)
+
+root.display()
+print()
+print(f" Answer: {max_in_tree(root)}")
+print()
+root.display()
