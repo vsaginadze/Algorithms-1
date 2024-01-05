4e38dd58dfc3a19f6137b2833e1979ec511ba707
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-05 23:52:40 +0400

    finish week-10 exercise-3

--- 10-week/exercises/3.py
+++ 10-week/exercises/3.py
@@ -1,14 +1,47 @@
 def valid_heap(arr):
-    pass
+    for i in range(len(arr), 3):
+        left, right, val = arr[2*i + 1], arr[2*i + 2], arr[i]
+        
+        if not parent_is_greater(val, left, right):
+            return False
+        
+    if not is_complete(arr):
+        return False
 
-# check if array is valid binary heap
-# what is valid binary heap?
+    return True
 
-    # is complete
-    # for every node except the very last the left node exists and right node exists
+def parent_is_greater(parent, left, right):
+    if parent > left or parent > right:
+        return False
+    return True
 
-    # left node and right node are less than parent
-    # parent = i
-    # left -> 2i + 1
-    # right -> 2i + 2
-    # 
+def is_heap_complete(arr):
+    depth, idx = max_depth(arr)
+    num_of_items = 2**depth
+
+    if len(arr[idx:]) == num_of_items or (num_of_items-1) == len(arr[idx:]):
+        return True
+    return False
+
+def is_complete(arr):
+    depth, idx = max_depth(arr)
+    
+    num_of_items = 2**depth
+
+    if len(arr[idx:]) == num_of_items or (num_of_items-1) == len(arr[idx:]):
+        return True
+    return False
+
+def max_depth(arr):
+    depth, left = 0, 0
+    while left < len(arr) and arr[left] is not None:
+        left = 2*left + 1
+        depth += 1
+    return (depth - 1, (left-1)//2)
+    
+arr = [2, 5, 3, 11, 7, 6, 4, 14, 12, 10, 9, 13, 6, 8]
+
+if valid_heap(arr):
+    print("Valid heap")
+else:
+    print("Invalid heap")
