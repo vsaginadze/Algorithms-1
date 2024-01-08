81e2626804f9a02bf35fe3684bc01a3daf198464
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-08 14:01:24 +0400

    week-11 DFS

--- 11-week/dfs.py
+++ 11-week/dfs.py
@@ -1,24 +1,57 @@
+# def dfs(g, start):
+#     visited = [False] * len(g)
+
+#     def visit(v):
+#         print(f'visiting {v}')
+#         visited[v] = True
+#         for w in g[v]:
+#             if not visited[w]:
+#                 visit(w)
+    
+#     visit(start)
+
+
 def dfs(g, start):
     visited = [False] * len(g)
-    
+
     def visit(v):
         print(f'visiting {v}')
         visited[v] = True
         for w in g[v]:
             if not visited[w]:
                 visit(w)
-    
     visit(start)
 
-# adjacency list representation
-graph = [
-    [1, 2, 5, 6],
-    [0],
-    [0],
-    [4, 5],
-    [3, 5, 6],
-    [0, 3, 4],
-    [0, 4]
-]
 
-dfs(graph, 0)
+# # adjacency list representation
+# graph = [
+#     [1, 2, 5, 6],
+#     [0],
+#     [0],
+#     [4, 5],
+#     [3, 5, 6],
+#     [0, 3, 4],
+#     [0, 4]
+# ]
+
+# dfs(graph, 0)
+
+# from read_graph import read_graph
+
+# europe = read_graph()
+# print(europe)
+
+# def dfs(g, start):
+#     visited = set()
+
+#     def visit(v, depth):
+#         print('  ' * depth + v)
+#         visited.add(v)
+#         for w in g[v]:
+#             if w not in visited:
+#                 visit(w, depth + 1)
+    
+#     visit(start, 0)
+
+# europe = read_graph()
+# dfs(europe, 'ireland')
