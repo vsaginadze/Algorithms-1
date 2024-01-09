24c6d32d40da664cc409224780866bc109971381
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-09 19:51:49 +0400

    just making my github greener

--- 11-week/searching_in_state_space.py
+++ 11-week/searching_in_state_space.py
@@ -7,21 +7,20 @@ def knight_path(start_x, start_y, end_x, end_y):
     visited = [[False] * 8 for i in range(8)]
 
     q = deque()
-    q.append( (start_x, start_y, 0))
+    q.append((start_x, start_y, 0))
     visited[start_x][start_y] = True
 
     while len(q) > 0:
         x, y, dist = q.popleft()
-        print(f'visiting {(x, y)}')
-        if (x, y) == (end_x, end_y):
-            print(f'shortest distance is {dist}')
+        if x == end_x and y == end_y:
+            print(f"shortest distance is {dist}")
             return
         
         for dx, dy in dirs:
-            x1, y1 = x + dx, y + dy
+            x1, y1 = x + dx, y+dy
             if 0 <= x1 < 8 and 0 <= y1 < 8 and not visited[x1][y1]:
-                q.append( (x1, y1, dist+1) )
-                visited[x1][y1] = True
-    print('Unreachable')
+                q.append((x1, y1, dist+1))
+                visited[x1][y1]
+    print('unreachable')
 
 knight_path(4, 4, 1, 5)
