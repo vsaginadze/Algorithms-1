2281897f2153be4c7ab0de6e98b7493d26f33666
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-09 12:38:42 +0400

    week-11 searching in state space

---
+++ 11-week/searching in state space.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+dirs = [(1, 2), (-1, 2), (1, -2), (-1, -2),
+        (2, 1), (2, -1), (-2, 1), (-2, -1)]
+
+def knight_path(start_x, start_y, end_x, end_y):
+    visited = [[False] * 8 for i in range(8)]
+
+    q = deque()
+    q.append( (start_x, start_y, 0))
+    visited[start_x][start_y] = True
+
+    while len(q) > 0:
+        x, y, dist = q.popleft()
+        print(f'visiting {(x, y)}')
+        if (x, y) == (end_x, end_y):
+            print(f'shortest distance is {dist}')
+            return
+        
+        for dx, dy in dirs:
+            x1, y1 = x + dx, y + dy
+            if 0 <= x1 < 8 and 0 <= y1 < 8 and not visited[x1][y1]:
+                q.append( (x1, y1, dist+1) )
+                visited[x1][y1] = True
+    print('Unreachable')
+
+knight_path(4, 4, 1, 5)
