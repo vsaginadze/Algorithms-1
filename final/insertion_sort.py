08ad474cc310cef9abd4864919c069fb3deecd29
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-14 15:21:13 +0400

    finish final insertion_sort

---
+++ final/insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > temp:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = temp
+    return arr
+arr = [-2, 5, 3, 1, -56, 7, 2, -3]
+print(insertion_sort(arr))
