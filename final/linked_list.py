54e803fe51b88956c4d737db67643fc50b76ad45
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-17 02:35:40 +0400

    finish final linked_list

---
+++ final/linked_list.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    def __init__(self, head):
+        self.head = head
+    
+    
