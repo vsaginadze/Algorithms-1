aab595a1465a4c5ad66b8ad27cb35b72dfdbe356
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-17 16:50:49 +0400

    make github greener

--- 8-week/map.py
+++ 8-week/map.py
@@ -29,7 +29,7 @@ class Map:
 
         if p is None:
             self.root = pair
-            return 
+            return
 
         while True:
             if key == p.key:
@@ -47,36 +47,6 @@ class Map:
                 else:
                     p.right = pair
                     return # insert the pair if RIGHT DNE
-    
-    def remove(self, key):
-        '''
-        Deleting a value from a binary search tree is a bit trickier. 
-        It's not hard to find the node to delete: we just walk down the tree, 
-        just like when searching or inserting. Once we've found the node N we want to delete, 
-        there are several cases.
-            If N is a leaf (it has no children), we can just remove it from the tree.
-
-            If N has only a single child, we replace N with its child. For example, 
-            we can delete node 15 in the binary tree above by replacing it with 18.
-
-            If N has two children, then we will replace its value by the next highest value in the tree. 
-            To do this, we start at N's right child and follow left child pointers for as long as we can. 
-            This wil take us to the smallest node in N's right subtree, 
-            which must be the next highest node in the tree after N.
-            Call this node M. We can easily remove M from the right subtree: 
-            M has no left child, so we can remove it following either case (a) or (b) above. 
-            Now we set N's value to the value that M had.
-
-            As a concrete example, suppose that we want to delete the root node 
-            (with value 10) in the tree above. 
-            This node has two children. We start at its right child (20) and follow its 
-            left child pointer to 15. That’s as far as we can go in following left child pointers, 
-            since 15 has no left child. So now we remove 15 (following case b above), 
-            and then replace 10 with 15 at the root.
-
-        We won't give an implementation of this operation here, but writing this yourself is an excellent (and somewhat challenging) exercise.
-        '''
-
 
 my_map = Map()
 
