dbfc53afa70a11c855e8ba7dc6534b6402ee0772
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-17 18:49:29 +0400

    finish final dynamic_array

---
+++ final/dynamic_array.py
@@ -0,0 +1,24 @@
+class DynamicArray:
+    def __init__(self):
+        self.a = 10 * [None]
+        self.n = 0
+    
+    def resize(self):
+        b = 2 * len(self.a) * [None]
+        for k in range(len(self.a)):
+            b[k] = self.a[k]
+        self.a = b
+    
+    def add(self, x):
+        if self.n >= len(self.a):
+            self.resize()
+        self.a[self.n] = x
+        self.n += 1
+    
+    def set(self, i, x):
+        if i >= self.n:
+            return "Error"
+        self.a[i] = x
+    
+    def get(self, i):
+        return self.a[i]
