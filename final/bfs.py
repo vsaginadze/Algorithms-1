405db68fe5c07f9fee74c3175c712b5589d709cf
Vakhtang Saginadze <vakhtang_saginadze@protonmail.com>
2024-01-21 18:17:47 +0400

    finish final graphs bfs

---
+++ final/bfs.py
@@ -0,0 +1,26 @@
+graph = [
+    [1, 2, 5, 6],    # neighbors of 0
+    [0],             # neighbors of 1
+    [0],             # …
+    [4, 5],
+    [3, 5, 6],
+    [0, 3, 4],
+    [0, 4]
+]
+
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    q = deque()
+
+    q.append((start, 0))
+    visited.add(start)
+
+    while len(q) > 0:
+        v, dist = q.popleft()
+        print('   ' * dist + v)
+        for w in graph[v]:
+            if w not in visited:
+                q.append((w, dist + 1))
+                visited.add(w)
